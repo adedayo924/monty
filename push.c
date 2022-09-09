bd98e5e0a6187f06efe631e6490a841e54f337d9
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2022-09-09 05:30:45 +0100

    Solutions to Stacks and Queues tasks

--- push.c
+++ push.c
@@ -7,15 +7,15 @@
 */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, k = 0, flag = 0;
+	int n, j = 0, flag = 0;
 
 	if (bus.arg)
 	{
 		if (bus.arg[0] == '-')
-			k++;
-		for (; bus.arg[k] != '\0'; k++)
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
 		{
-			if (bus.arg[k] > 57 || bus.arg[k] < 48)
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
 				flag = 1; }
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
